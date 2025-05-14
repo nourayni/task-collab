367e761a7f3780e5e93d0deff662ab500035b422
ousmane drame <odrame68@gmail.com>
2025-05-14 12:48:24 +0000

    l'interface de projecteService

---
+++ src/main/java/com/taskcolab/project/service/ProjectService.java
@@ -0,0 +1,18 @@
+package com.taskcolab.project.service;
+
+import java.util.List;
+
+import com.taskcolab.project.dto.ProjectDTO;
+import com.taskcolab.project.dto.ProjectStatsDTO;
+
+public interface ProjectService {
+    ProjectDTO createProject(ProjectDTO projectDTO);
+    ProjectDTO getProject(Long id);
+    List<ProjectDTO> getAlProjects();
+    ProjectDTO updateProject(Long id, ProjectDTO projectDTO);
+    void deleteProject(Long id);
+    void addMember(Long projectId, Long userId);
+    List<ProjectDTO> getUserProject(String email);
+    ProjectDTO archiveProject(Long id, boolean archive);
+    ProjectStatsDTO getProjectStats(Long id);
+}
